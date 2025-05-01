7d5e7eb6a4f34be801ed51d136571f8b2888bc32
Will Stansill <willstansill@gmail.com>
2025-05-01 11:48:51 -0400

    added getter functions that can detail things like swim data and rivers according to class

--- src/RiverLog.sol
+++ src/RiverLog.sol
@@ -29,7 +29,11 @@ contract RiverLog {
     }
 
     KayakTrip[] public kayakRiverLog;
+    KayakTrip[] public kayakSwims;
     RaftTrip[] public raftRiverLog;
+    RaftTrip[] public raftSwims;
+
+    mapping(uint8 => string[]) public riversByClass;
 
     function addKayakTrip(
         uint8 _date,
@@ -48,6 +52,10 @@ contract RiverLog {
             durationHours: _durationHours
         });
         kayakRiverLog.push(newKayakTrip);
+        riversByClass[_riverClass].push(_riverName);
+        if (_swim == true) {
+            kayakSwims.push(newKayakTrip);
+        }
     }
 
     function addRaftingTrip(
@@ -68,6 +76,52 @@ contract RiverLog {
             navStyle: _navStyle,
             durationHours: _durationHours
         });
+        if (_swim == true) {
+            raftSwims.push(newRaftingTrip);
+        }
+        riversByClass[_riverClass].push(_riverName);
         raftRiverLog.push(newRaftingTrip);
+    }
+
+    function getTotalKayakTrips()
+        public
+        view
+        returns (uint256 totalKayakTrips)
+    {
+        totalKayakTrips = kayakRiverLog.length;
+        return totalKayakTrips;
+    }
+
+    function getTotalRaftTrips() public view returns (uint256 totalRaftTrips) {
+        totalRaftTrips = raftRiverLog.length;
+        return totalRaftTrips;
+    }
+
+    function getTotalTripsTaken()
+        public
+        view
+        returns (uint256 totalTripsTaken)
+    {
+        totalTripsTaken = (getTotalKayakTrips() + getTotalRaftTrips());
+        return totalTripsTaken;
+    }
+
+    function getRiversAccordingToClass(
+        uint8 riverClass
+    ) public view returns (string[] memory) {
+        return riversByClass[riverClass];
+    }
+
+    function getSwimData()
+        public
+        view
+        returns (
+            uint256 totalSwims,
+            KayakTrip[] memory kayaks,
+            RaftTrip[] memory rafts
+        )
+    {
+        totalSwims = (kayakSwims.length) + (raftSwims.length);
+        return (totalSwims, kayakSwims, raftSwims);
     }
 }
